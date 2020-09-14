016ddef3c0014ccd9a29e56649c41048eba1469b
llnsun <1090146275@qq.com>
2020-09-14 22:53:47 +0800

    addfile

---
+++ app.vue
@@ -0,0 +1,28 @@
+import mxgraph from "../../graph/index";
+
+const {
+  mxGraph,
+  mxClient,
+  mxDragSource,
+  mxCell,
+  mxRubberband,
+  mxVertexHandler,
+  mxConstants,
+  mxCellState,
+  mxPerimeter,
+  mxCellEditor,
+  mxGraphHandler,
+  mxEvent,
+  mxEdgeHandler,
+  mxShape,
+  mxConnectionConstraint,
+  mxPoint,
+  mxEventObject,
+  mxCodec,
+  mxObjectCodec,
+  mxUtils,
+  mxImageExport,
+  mxXmlCanvas2D,
+  mxClipboard,
+  mxCodecRegistry
+} = mxgraph;
